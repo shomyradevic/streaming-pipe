2eabe16b15fdada31db74b2120cf81c517a0bda0
Milos <milos.radevic3@gmail.com>
2025-06-19 14:05:11 +0200

    Optimized main producer file

--- producer/main/main.go
+++ producer/main/main.go
@@ -10,76 +10,58 @@ import (
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-/*
-1. Connect to a kafka topic
-2. For each tweet field, generate a random value
-3. In an infinite loop -> send payloads to the kafka topic
-		- Figure out worker count
-4. Produce logs to the logs/
-*/
-
-func createAndSendToTopic(topicName string) {
-
+func createMessage() map[string]any {
 	var edited = [2]bool{true, false}
-
 	seed := rand.New(rand.NewSource(time.Now().UnixNano()))
-
 	tweet := map[string]any{
-		"id":   producer.GenerateRandomInteger(1000000, 10000000, *seed),
-		"text": producer.GetRandomString(producer.GenerateRandomInteger(1, 10+1, *seed)),
-		// This needs to be fixed - We are doing I/O at every iteration, which doesnt make sense. It takes ~ 17 secs for 100K messages, which is a lot.
-
+		"id":         producer.GenerateRandomInteger(1000000, 10000000, *seed),
+		"text":       producer.GetRandomString(producer.GenerateRandomInteger(1, 10+1, *seed)),
 		"author_id":  producer.GenerateRandomInteger(100000, 1000000, *seed),
 		"retweets":   producer.GenerateRandomInteger(10000, 100000, *seed),
 		"likes":      producer.GenerateRandomInteger(100, 1000, *seed),
 		"created_at": producer.GenerateRandomDateTime(),
 		"edited":     edited[producer.GenerateRandomInteger(0, 1, *seed)]}
-
-	sendToKafkaTopic(tweet, topicName)
-
+	return tweet
 }
 
-func sendToKafkaTopic(tweet map[string]any, topicName string) {
-
-	formatted, err := json.MarshalIndent(tweet, "", "  ")
+func sendToKafkaTopic(message map[string]any, messageIndex int, topicName string, kafkaProducer *kafka.Producer) {
+	formatted, err := json.MarshalIndent(message, "", "  ")
 	if err != nil {
 		fmt.Println("error:", err)
 	}
-	fmt.Println("tweet:", string(formatted))
 
-	producer, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "kafka:9092"})
-	if err != nil {
-		fmt.Println("Failed to create producer!")
-	}
-	defer producer.Close()
-
-	err = producer.Produce(&kafka.Message{
+	err = kafkaProducer.Produce(&kafka.Message{
 		TopicPartition: kafka.TopicPartition{Topic: &topicName, Partition: int32(-1)}, // equivalent to kafka.PartitionAny
 		Value:          formatted,
 	}, nil)
 
-	if err != nil {
-		fmt.Println("Error", err)
+	if err == nil {
+		fmt.Println("Message sent:", messageIndex)
+	} else {
+		fmt.Println("Error while sending %d. message:", err)
 	}
-
-	producer.Flush(5000)
-
 }
 
-func run(workerCount uint8, messageCount int) {
+func run(workerCount uint8, messageCount int, kafkaProducer *kafka.Producer, topicName string) {
 	fmt.Println("Using workers: ", workerCount)
 	for i := 0; i < messageCount; i++ {
-		createAndSendToTopic("raw-topic")
-		// time.Sleep(time.Second)
+		message := createMessage()
+		sendToKafkaTopic(message, i, topicName, kafkaProducer)
 	}
 }
 
 func main() {
-	defer fmt.Println("Program finished with execution!") // :)
+	var workerCount uint8 = 1
+	var messageCount int = 100000
+	var kafkaProducer = producer.CreateProducer()
+	var topicName string = "raw-topic"
+
+	defer kafkaProducer.Flush(5000)
+	defer kafkaProducer.Close()
 
 	start := time.Now()
 
-	run(1, 100000)
+	run(workerCount, messageCount, kafkaProducer, topicName)
 
 	elapsed := time.Since(start)
 	fmt.Println("Execution time: ", elapsed)
