1461733054178d67c88c6dd75dd8b9f2bfe2d7f9
Milos <milos.radevic3@gmail.com>
2025-06-25 14:18:19 +0200

    Just debugging hell

--- producer/main/main.go
+++ producer/main/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 	"producer/producer/producer"
+	"sync"
 	"syscall"
 	"time"
 
@@ -12,39 +13,56 @@ import (
 )
 
 func run(workerCount uint8, messageCount int, kafkaProducer *kafka.Producer, topicName string) {
+
+	// Trap SIGINT/SIGTERM so we can flush before exiting
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	var wg sync.WaitGroup // This can be important
+
 	fmt.Println("Using workers: ", workerCount)
 
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
-
-	/*
-		Looks like this goroutine doesnt prevent producer from just silently stopping at about 30K messages...
-		Maybe its a different signal? We need to investigate whats actually happening and why its just casually stopping...
-	*/
 	go func() {
-		<-sigchan
-		fmt.Println("Flushing...")
-		kafkaProducer.Flush(5000)
-		kafkaProducer.Close()
-		os.Exit(0)
+		for e := range kafkaProducer.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition.Error)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
 	}()
 
-	for i := 0; i < messageCount; i++ {
-		message := producer.CreateMessage()
-		producer.SendToKafkaTopic(message, i, topicName, kafkaProducer)
-	}
-	kafkaProducer.Flush(5000)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 0; i < messageCount; i++ {
+			message := producer.CreateMessage()
+			producer.SendToKafkaTopic(message, i, topicName, kafkaProducer)
+		}
+	}()
+	wg.Wait()
+
+	fmt.Printf("Finished with loop!")
+	remaining := kafkaProducer.Flush(5000)
+	fmt.Printf("Remaining after flush: %d", remaining)
+
+	fmt.Println("🏁 All done. Sleeping 2s before exit to flush OS/Docker buffers...")
+	/* With this, everything is printed when using 5000 messages, which means that docker is early exiting.
+	However, when doing 200K messages, it doesnt print everything, because it early exits.*/
+	time.Sleep(5 * time.Second)
+
 	kafkaProducer.Close()
+
 }
 
 func main() {
 	var workerCount uint8 = 1
-	var messageCount int = 100000
+	var messageCount int = 200000
 	var kafkaProducer = producer.CreateProducer()
 	var topicName string = "raw-topic"
-
-	// defer kafkaProducer.Flush(5000)
-	// defer kafkaProducer.Close()
 
 	start := time.Now()
 
@@ -52,4 +70,5 @@ func main() {
 
 	elapsed := time.Since(start)
 	fmt.Println("Execution time: ", elapsed)
+
 }
